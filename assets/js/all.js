902b85f22e7689e7e8b4a9befafa866b481aa7cb
SuPan <joelopan@gmail.com>
2020-12-23 01:01:47 +0800

    Update 2020-12-22T17:01:28.487Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,5 +3,38 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $(function () {
+    $("#from").datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function onClose(selectedDate) {
+        $("#to").datepicker("option", "minDate", selectedDate);
+      }
+    });
+    $("#to").datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function onClose(selectedDate) {
+        $("#from").datepicker("option", "maxDate", selectedDate);
+      }
+    });
+  });
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
