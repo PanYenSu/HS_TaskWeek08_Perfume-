a4536c99cef37a1f51c76c1ee93a0d76b7e8ce82
SuPan <joelopan@gmail.com>
2020-12-30 17:32:34 +0800

    Update 2020-12-30T09:32:27.459Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,6 @@
 "use strict";
 
 console.log('Hello!');
-AOS.init();
 $(document).ready(function () {
   console.log('HesSchool Hello!');
   $(function () {
@@ -22,17 +21,27 @@ $(document).ready(function () {
       }
     });
   });
+  AOS.init({
+    offset: 120,
+    // offset (in px) from the original trigger point
+    delay: 0,
+    // values from 0 to 3000, with step 50ms
+    duration: 1000,
+    // values from 0 to 3000, with step 50ms
+    anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+  });
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 1,
     // 重複輪播
     loop: true,
-    // loopFillGroupWithBlank: true,
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    // },
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
